18d97ac6ef2d32dd885248775c80957d6fe9a2c9
volodink <volodin.konstantin@gmail.com>
2015-11-30 01:39:58 +0300

    Added some files.

--- article/regression/test.R
+++ article/regression/test.R
@@ -1,4 +1,16 @@
 mydata <- read.csv('data/narez.csv', sep=',')
-fit <- lm(mydata$y ~ mydata$x )
+x = mydata$x
+y = mydata$y
+fit <- lm(y ~ x)
 summary(fit)
+par(cex=.8)
 
+pdf(file='images/firstplot.pdf')
+  plot(x, y, col='blue', main='My First Chart')
+  abline(fit, col='red')
+dev.off()
+
+pdf(file='images/linearfit1.pdf')
+  par(mfrow=c(2,2))
+  plot(fit)
+dev.off()
